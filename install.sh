54535a244951f4f8f3c9cb9bb5dc344bfb19096e
mohan-bit1 <skysys.mohan@gmail.com>
2026-05-19 13:19:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -217,10 +217,10 @@ check_python() {
     PYTHON="python"
   fi
 
-  if [[ $PYTHONVERSION != "2.7" ]]; then
-    echo "Python version 2.7 is required to install pty.js. Please install python 2.7 and try again. You can find more information on how to install Python in the docs: https://docs.aws.amazon.com/cloud9/latest/user-guide/ssh-settings.html#ssh-settings-requirements"
-    exit 100
-  fi
+  #if [[ $PYTHONVERSION != "2.7" ]]; then
+  #  echo "Python version 2.7 is required to install pty.js. Please install python 2.7 and try again. You can find more information on how to install Python in the docs: https://docs.aws.amazon.com/cloud9/latest/user-guide/ssh-settings.html#ssh-settings-requirements"
+  #  exit 100
+  #fi
 }
 
 # NodeJS
